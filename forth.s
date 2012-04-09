d9d6cc637fd2e9ef1f5dc03ad677b10de331bdc0
Vyacheslav Barinov <v.barinov@samsung.com>
2012-04-09 15:02:04 +0400

    Work is going on

--- forth.s
+++ forth.s
@@ -1,10 +1,17 @@
 jmp start
 
 ; some kind of configuration
-	:dictionary_start		
+	:dictionary_end		
 		dat 0,0,0,0
 	:return_stack_top		
-		dat 0,0,0,3
+		dat 0,0,0,0
+	:return
+		dat return_stack_top
+
+	:cmd1
+		dat "dup"
+	:cmd2
+		dat "rot"
 
 ; data section here
 	:initial_stack 			
@@ -18,13 +25,41 @@ jmp start
 
 :start
 	mov [return_stack_top], 0x3000 ; init return sp
+	mov [dictionary_end], upstr ; last word on boot
 
-	set push, result
-	set push, 12345
-	callword(inttostr)
-	callword(writeline)
+	mov push, cmd2
+	callword(searchForWord)
 
 	jmp exit
+
+defword(searchForWord, 0, searchForWord)
+	mov [sfw_local_name_str], pop
+	jmp sfw_begin
+
+	:sfw_local_current_addr
+		dat 0,0,0,0
+	:sfw_local_name_str
+		dat 0,0,0,0
+	:sfw_local_name_len
+		dat 0,0,0,0
+	
+	:sfw_begin
+		mov [sfw_local_current_addr], [rot]
+		add [sfw_local_current_addr], 1 ; Last word name
+
+		mov push, [sfw_local_name_str]
+		callword(upstr)
+		mov a,b
+		mov b,a
+		callword(strlen)
+		mov [sfw_local_name_len], pop			; X = name_length
+
+;		:sfw_mainloop
+;			mov b, [sfw_local_current_addr]
+;			callword(strcmp)
+;			mov J, pop
+
+next
 
 defword(dup, 0, dup)
 	set a, pop
@@ -42,7 +77,21 @@ defword(rot, 0, rot)
 next
 
 defword(strcmp, 0, strcmp)
+	mov a, pop
+	mov b, pop
 
+	mov i, 0
+	:strcmp_loop
+		ifn [a],[b]
+		jmp strcmp_exit
+		add a, 1
+		add b, 1
+		add i, 1
+		ifn [a], 0
+		jmp strcmp_loop
+
+	:strcmp_exit
+	push i
 next
 
 defword(writeline, 0, writeline)
@@ -68,10 +117,11 @@ defword(strlen, 0, strlen)
 		add a,1
 		jmp __strlen_loop
 	:__strlen_exit
+
 	set push, i
 next
 
-defword(i2s, 0, inttostr)
+defword(inttostr, 0, inttostr)
 	mov a, pop
 	mov b, pop
 	mov z, b
@@ -99,6 +149,30 @@ defword(i2s, 0, inttostr)
 		jmp __next_digit
 	set push, z
 next
+
+defword(upstr, 0, upstr)
+	mov a, pop
+	mov z, a
+	jmp __upstr_begin
+
+	:__upstr_loop
+		add a, 1
+	:__upstr_begin
+		ife [a], 0
+		jmp __upstr_exit
+
+		ifg	0x60, [a]
+		jmp __upstr_loop
+		ifg [a], 0x7a
+		jmp __upstr_loop
+
+		sub [a], 0x20
+		jmp __upstr_loop
+
+	:__upstr_exit
+	set push, z
+next
+
 
 :exit
 	dat 0x0000
--- forth_macro.m4
+++ forth_macro.m4
@@ -1,9 +1,11 @@
+define(`prev_word', `0')
+
 define(`defword', `:$3
-  DAT "upcase($1)", 0
-  DAT len($1)
-  DAT $2
+  DAT ifelse(`prev_word', `0', `0,0,0,0' , `prev_word') ; Link to previous
+  DAT "upcase($1)", 0 ; Name
+  DAT $2 ; Flags
   :__$3_start
-  SET [X], POP')
+  SET [X], POP define(`prev_word', $3)')
 
 define(`callword', ` ADD [return_stack_top], 4
  SET X, [return_stack_top]
@@ -13,3 +15,4 @@ define(`next', ` SET X, [return_stack_top]
  SET PUSH, [X]
  SUB [return_stack_top], 4
  SET PC, POP')
+
