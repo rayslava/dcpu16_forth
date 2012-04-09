efe7a3fde21ab6ee09e2f44b33883dd542551357
Vyacheslav Barinov <v.barinov@samsung.com>
2012-04-09 13:26:45 +0400

    Some new words

--- forth.s
+++ forth.s
@@ -1,26 +1,30 @@
 jmp start
 
 ; some kind of configuration
-	:dictionary_start		dat 0,0,0,0
-	:return_stack_top		dat 0,0,0,3
+	:dictionary_start		
+		dat 0,0,0,0
+	:return_stack_top		
+		dat 0,0,0,3
 
 ; data section here
-	:initial_stack 			dat 0x0000
+	:initial_stack 			
+		dat 0x0000
+	:prompt
+		dat "FORTH> ",0
+	:result
+		dat "0000000",0
+	:ok_msg
+		dat "Ok",0
 
 :start
 	mov	initial_stack, sp 	; saving initial stack pointer if we should inline into other program (OS)
-	mov I, return_stack_top
-	mov [I], 0x3000
+	mov [return_stack_top], 0x3000
 
+	set push, result
+	set push, 12345
+	callword(inttostr)
+	callword(writeline)
 
-	set push, 2
-	callword(dup)
-	set push, 3
-	callword(rot)
-	set a, pop
-	set b, pop
-	set c, pop
-	
 	jmp exit
 
 defword(dup, 0, dup)
@@ -36,6 +40,66 @@ defword(rot, 0, rot)
 	set push, b
 	set push, a
 	set push, c
+	callword(dup)
+next
+
+defword(strcmp, 0, strcmp)
+
+next
+
+defword(writeline, 0, writeline)
+	mov a, pop
+	mov i, 0
+
+	:__writeline_loop1
+		mov [0x8000+i], [a]
+		add i, 1
+		add a, 1
+		ifn [a], 0
+		jmp __writeline_loop1
+next
+
+defword(strlen, 0, strlen)
+	mov a, pop
+	mov i, 0
+
+	:__strlen_loop
+		ife [a], 0
+		jmp __strlen_exit
+		add i, 1
+		add a,1
+		jmp __strlen_loop
+	:__strlen_exit
+	set push, i
+next
+
+defword(i2s, 0, inttostr)
+	mov a, pop
+	mov b, pop
+	mov z, b
+	mov x, a
+
+	mov y, 10000
+	jmp __next_digit_start
+
+	:__digit		
+		add x, 48
+		
+		mov [b],x
+		add b, 1
+		ret
+
+	:__next_digit
+		mov x, a
+		mod x, y
+		div y, 10
+
+	:__next_digit_start 
+		div x, y
+		jsr __digit
+		ifg y, 1
+		jmp __next_digit
+	set push, z
 next
 
 :exit
--- forth_macro.m4
+++ forth_macro.m4
@@ -2,16 +2,14 @@ define(`defword', `:$3
   DAT "upcase($1)", 0
   DAT len($1)
   DAT $2
-  __$3_start:
-  SET [Y], POP')
+  :__$3_start
+  SET [X], POP')
 
-define(`callword', ` SET X, return_stack_top 
- ADD [X], 4
- SET Y, [X]
+define(`callword', ` ADD [return_stack_top], 4
+ SET X, [return_stack_top]
  jsr __$1_start')
 
-define(`next', ` SET X, return_stack_top
- SET Y, [X]
- SET PUSH, [Y]
- SUB [X], 4
+define(`next', ` SET X, [return_stack_top]
+ SET PUSH, [X]
+ SUB [return_stack_top], 4
  SET PC, POP')
